c4221a3af074cd64750a3b40c803060710d75f8f
pgonzalezs1999 <pgonzalezs1999@gmail.com>
2020-11-09 13:34:53 +0100

    Algo mejora, pero sigue el bucle infinito

--- main.cpp
+++ main.cpp
@@ -2,48 +2,11 @@
 #include <fstream>
 using namespace std;
 
-struct alumno
+void Mostrar(string horaCreacion, string correo, string nombre, string apellido, bool esRepetidor, int expediente, string gitHub, int nivelC, bool haPresentado, bool entregadoAct, int mesNacimiento, int yearNacimiento, bool estaPresencial)
 {
-    string nombre;
-    string apellido;
-    string correo;
-    string gitHub;
-    bool esRepetidor; // 0 = repetidor; 1 = nuevo alumno;
-    bool estaPresencial; // 0 = presencial; 1 = online
-    bool haPresentado; // 0 = ya ha expuesto; 1 = pendiente de exponer;
-    bool entregadoActividad; // 0 = sí ha entregado; 1 = no ha entregado;
-    int expediente;
-    int nivelC; // 0 = piltrafilla; 1 = nivel dios;
-    int yearNacimiento;
-    int mesNacimiento;
-    string fechaCreacion;
-    string horaCreacion;
-    alumno *siguiente = NULL;
-};
-
-alumno *inicio = NULL;
-alumno *final = NULL;
-
-void Mostrar()
-{
-    alumno *aux = inicio;
-    if (inicio == NULL)
-    {
-        cout << endl << "Su lista esta vacia." << endl;
-    }
-    else
-    {
-        cout << endl << "Su lista es: ";
-        cout << aux -> nombre;
-        aux = aux -> siguiente;
-
-        while (aux != inicio)
-        {
-            cout << " -> " << aux -> nombre;
-            aux = aux -> siguiente;
-        }
-        cout << endl;
-    }
+    //ofstream fout("../alumnos_resultado.txt"); // Declaramos fout como variable de salida
+    cout << horaCreacion << correo << nombre << apellido << esRepetidor << expediente << gitHub <<
+         nivelC << haPresentado << entregadoAct << mesNacimiento << yearNacimiento << estaPresencial;
 }
 void AlumnoMasViejo()
 {
@@ -71,47 +34,41 @@ void ListaPresencialidad()
 }
 int main()
 {
-
+    string nombre;
+    string apellido;
+    string correo;
+    string gitHub;
+    bool esRepetidor; // 0 = repetidor; 1 = nuevo alumno;
+    bool estaPresencial; // 0 = presencial; 1 = online
+    bool haPresentado; // 0 = ya ha expuesto; 1 = pendiente de exponer;
+    bool entregadoAct; // 0 = sí ha entregado; 1 = no ha entregado;
+    int expediente;
+    int nivelC; // 0 = piltrafilla; 1 = nivel dios;
+    int yearNacimiento;
+    int mesNacimiento;
+    string fechaCreacion;
+    string horaCreacion;
     int totalUsuarios = 0;
     string fechaActual;
-    alumno *aux = new alumno;
 
     ifstream fin("../alumnos.tsv"); // Declaramos fin como variable de entrada
-    ofstream fout("../alumnos_pruebas.txt"); // Declaramos fout como variable de salida
-    ofstream fbin("../alumnos_pruebas.tsv", ios::out | ios::binary); // Variable binaria
-    fin >> aux -> fechaCreacion; // leemos un campo para saber si estamos en fin de archivo
+    ofstream fout("../alumnos_resultado.txt"); // Declaramos fout como variable de salida
+    ofstream fbin("../alumnos_resultado.tsv", ios::out | ios::binary); // Variable binaria
+    fin >> fechaCreacion; // leemos un campo para saber si estamos en fin de archivo
     try
     {
         while(!fin.eof())
         {
+            fin >> horaCreacion >> correo >> nombre >> apellido >> esRepetidor >> expediente >> gitHub >>
+            nivelC >> haPresentado >> entregadoAct >> mesNacimiento >> yearNacimiento >> estaPresencial;
 
-            fin >> aux -> horaCreacion >> aux -> correo >> aux -> nombre >> aux -> apellido >>
-            aux -> esRepetidor >> aux -> expediente >> aux -> gitHub >> aux -> nivelC >>aux -> haPresentado >>
-            aux -> entregadoActividad >> aux -> mesNacimiento >> aux -> yearNacimiento >> aux -> estaPresencial;
+            Mostrar(horaCreacion, correo, nombre, apellido, esRepetidor, expediente, gitHub, nivelC, haPresentado, entregadoAct, mesNacimiento, yearNacimiento, estaPresencial);
 
-            if (inicio == NULL) { inicio = aux; }
-            if (final == NULL) { final == aux; }
-            else
-            {
-                final -> siguiente = aux;
-                final = aux;
-            }
-
-            //cout << "ACEPTADO: " << nombre << ", con renta " << renta << " y nota "
-                 //<< nota << endl; // Imprimimos los aceptados
-            fout << aux -> nombre << " " << aux -> expediente << " " << aux -> estaPresencial << endl;
-            // Hacemos cast y escribimos en alumnos_pruebas.tsv
-            fbin.write(reinterpret_cast<const char *>(&aux->nombre), sizeof(string));
-            fbin.write(reinterpret_cast<const char *>(&aux->expediente), sizeof(string));
-            fbin.write(reinterpret_cast<const char *>(&aux->correo), sizeof(string));
-
-            cout << "Llego hasta aqui" << endl;
-            totalUsuarios++;
-            fin >> aux -> fechaCreacion; // Leemos para saber si estamos en fin de archivo
+            // fbin.write(reinterpret_cast<const char *>(&nombre), sizeof(string)); // Hacemos cast y escribimos en alumnos_pruebas.tsv
+            // fbin.write(reinterpret_cast<const char *>(&expediente), sizeof(string));
+            // fbin.write(reinterpret_cast<const char *>(&correo), sizeof(string));
+            fin >> fechaCreacion; // Leemos para saber si estamos en fin de archivo
         }
-
-        Mostrar();
-
     } // Cerramos archivos de texto y binarios
     catch(...) // Captura todas las excepciones posibles
     {
