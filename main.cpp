5ae9c934ecb975852181a766fd66571513a27e9e
pgonzalezs1999 <pgonzalezs1999@gmail.com>
2020-11-30 19:03:59 +0100

    Entrega examen

--- main.cpp
+++ main.cpp
@@ -2,184 +2,44 @@
 #include <fstream>
 using namespace std;
 
-struct alumno
+struct municipio
 {
+    int codigo;
     string nombre;
-    string apellido;
-    string correo;
-    string gitHub;
-    bool esRepetidor; // 0 = repetidor; 1 = nuevo alumno;
-    bool estaPresencial; // 0 = presencial; 1 = online
-    bool haPresentado; // 0 = ya ha expuesto; 1 = pendiente de exponer;
-    bool entregadoAct; // 0 = sí ha entregado; 1 = no ha entregado;
-    int expediente;
-    int nivelC; // 0 = piltrafilla; 1 = nivel dios;
-    int year;
-    int mes;
-    string fechaCreacion;
-    string horaCreacion;
-    string fechaActual;
-    alumno *siguiente = NULL;
+    float poblacion;
+    float altitud;
+    float superficie;
+    municipio *siguiente;
 };
 
-alumno *inicio = NULL;
-alumno *final = NULL;
-int numeroAlumnos;
-ifstream fin("../alumnos.tsv.tsv"); // Declaramos fin como variable de entrada
-ofstream fout("../alumnos_resultado.txt"); // Declaramos fout como variable de salida
+municipio *inicio = NULL;
+municipio *final = NULL;
+ifstream fin("../examen.tsv"); // Declaramos fin como variable de entrada
+ofstream fout("../examen_resultado.txt"); // Declaramos fout como variable de salida
 
-void Mostrar()
+int LeerArchivo()
 {
-    alumno *aux = inicio;
-    while (aux != final)
-    {
-        fout << aux -> horaCreacion << " " << aux -> correo << " " << aux -> nombre << " " << aux -> apellido << " " << aux -> esRepetidor << " " << aux -> expediente << " " << aux -> gitHub << " " << aux -> nivelC << " " << aux -> haPresentado << " " << aux -> entregadoAct << " " << aux -> mes << " " << aux -> year << " " << aux -> estaPresencial << endl;
-        aux = aux -> siguiente;
-    }
-    fout << aux -> horaCreacion << " " << aux -> correo << " " << aux -> nombre << " " << aux -> apellido << " " << aux -> esRepetidor << " " << aux -> expediente << " " << aux -> gitHub << " " << aux -> nivelC << " " << aux -> haPresentado << " " << aux -> entregadoAct << " " << aux -> mes << " " << aux -> year << " " << aux -> estaPresencial << endl;
-    fout << endl;
-}
-void AlumnoMasViejo()
-{
-    alumno *aux = inicio, *viejo;
-    viejo = inicio;
-    while (aux != NULL)
-    {
-        if (aux -> year == viejo -> year)
-        {
-            if (aux -> mes < viejo -> mes)
-            {
-                viejo = aux;
-            }
-        }
-        else if (aux -> year < viejo -> mes)
-        {
-            viejo = aux;
-        }
-        aux = aux -> siguiente;
-    }
-    fout << "El alumno mas viejo es " << viejo -> nombre << " " << viejo -> apellido;
-    fout << endl << endl;
-}
-void CumplesEnNoviembre()
-{
-    alumno *aux = inicio;
-    int numeroCumples = 0;
-    fout << "Estas son las personas que cumplen en noviembre: " << endl << endl;
-    while (aux != NULL)
-    {
-        if (aux -> mes == 11)
-        {
-            fout << aux -> nombre << " " << aux -> apellido << endl;
-            numeroCumples++;
-        }
-        aux = aux -> siguiente;
-    }
-    if (numeroCumples == 1) { fout << "1 alumno cumple este mes" << endl; }
-    else { fout << numeroCumples << " alumnos cumplen este mes" << endl; }
-    fout << endl;
-}
-void ExpedientesImpar()
-{
-    alumno *aux = inicio;
-    fout << "Estas son las personas que tienen un numero de expediente impar: " << endl << endl;
-    while (aux != NULL)
-    {
-        if (aux -> expediente % 2 != 0)
-        {
-            fout << aux -> nombre << " " << aux -> apellido << " -> " << aux -> expediente << endl;
-        }
-        aux = aux -> siguiente;
-    }
-    fout << endl;
-}
-void ExpedientesPar()
-{
-    alumno *aux = inicio;
-    fout << "Estas son las personas que tienen un numero de expediente impar: " << endl << endl;
-    while (aux != NULL)
-    {
-        if (aux -> expediente % 2 == 0)
-        {
-            fout << aux -> nombre << " " << aux -> apellido << " -> " << aux -> expediente << endl;
-        }
-        aux = aux -> siguiente;
-    }
-    fout << endl;
-}
-void PorcentajeEntregadosA1()
-{
-    float porcentaje;
-    alumno *aux = inicio;
+    int numMunicipios = 0;
+    municipio *aux = new municipio;
 
-    while (aux != NULL)
-    {
-        if (aux -> entregadoAct == 0)
-        {
-            porcentaje++;
-        }
-        aux = aux -> siguiente;
-    }
-    porcentaje = (porcentaje * 100) / numeroAlumnos;
-    fout << "El " << porcentaje << "% de los alumnos han entregado la actividad 1" << endl << endl;
-}
-void ListaPresencialidad()
-{
-    int num = 0;
-    alumno *aux = inicio;
-    fout << "Estos somos los valientes que acudimos presencialmente: " << endl << endl;
-    while (aux != NULL)
-    {
-        if (aux -> estaPresencial == 0)
-        {
-            fout << aux -> nombre << " " << aux -> apellido << endl;
-            num++;
-        }
-        aux = aux -> siguiente;
-    }
-    fout << endl << "Eramos " << num << " en clase" << endl;
-    fout << endl << "Estos son los alumnos que se siguieron la clase desde la camita: " << endl << endl;
-    aux = inicio;
-    num = 0;
-    while (aux != NULL)
-    {
-        if (aux -> estaPresencial == 1)
-        {
-            fout << aux -> nombre << " " << aux -> apellido << endl;
-            num++;
-        }
-        aux = aux -> siguiente;
-    }
-    fout << endl << "Eran " << num << " en casa" << endl << endl;
-    fout << endl;
-}
-void LeerArchivo()
-{
-    alumno *aux;
-    int totalUsuarios = 0;
+    fin >> aux -> codigo; // leemos un campo para saber si estamos en fin de archivo
 
-    ofstream fbin("../alumnos_resultado.tsv", ios::out | ios::binary); // Variable binaria
-    fin >> aux -> fechaCreacion; // leemos un campo para saber si estamos en fin de archivo
     try
     {
         while(!fin.eof())
         {
-            numeroAlumnos++;
-            aux = new alumno;
-            fin >> aux -> horaCreacion >> aux -> correo >> aux -> nombre >> aux -> apellido >> aux -> esRepetidor
-                >> aux -> expediente >> aux -> gitHub >> aux -> nivelC >> aux -> haPresentado >> aux -> entregadoAct
-                >> aux -> mes >> aux -> year >> aux -> estaPresencial;
+            aux = new municipio;
 
-            // fbin.write(reinterpret_cast<const char *>(&nombre), sizeof(string)); // Hacemos cast y escribimos en alumnos_pruebas.tsv
-            // fbin.write(reinterpret_cast<const char *>(&expediente), sizeof(string));
-            // fbin.write(reinterpret_cast<const char *>(&correo), sizeof(string));
-            fin >> aux->fechaCreacion; // Leemos para saber si estamos en fin de archivo
+            fin >> aux -> nombre >> aux -> poblacion >> aux -> altitud >> aux -> superficie;
+            numMunicipios++;
+
+            fin >> aux -> codigo; // Leemos para saber si estamos en fin de archivo
 
             if (inicio == NULL && final == NULL)
             {
                 inicio = aux;
                 final = aux;
-                aux->siguiente = NULL;
+                aux -> siguiente = NULL;
             }
             else
             {
@@ -190,21 +50,171 @@ void LeerArchivo()
     } // Cerramos archivos de texto y binarios
     catch(...) // Captura todas las excepciones
     {
-        fbin.close();
         fin.close();
         fout.close();
-        cout<< "FIN DE ARCHIVO / Se ha generado el archivo binario de aceptados" << endl;
+        cout << "FIN DE ARCHIVO / Se ha generado el archivo binario de aceptados" << endl;
+    }
+    fout << "Pregunta 1: Hay " << numMunicipios << " municipios en Avila" << endl;
+}
+
+void Mostrar()
+{
+    municipio *aux = inicio;
+    while (aux != final)
+    {
+        fout << aux -> codigo << " " << aux -> nombre << " " << aux -> poblacion << " " << aux -> altitud << " " << aux -> superficie << endl;
+        aux = aux -> siguiente;
+    }
+    fout << aux -> codigo << " " << aux -> nombre << " " << aux -> poblacion << " " << aux -> altitud << " " << aux -> superficie << endl;
+
+    fout << endl;
+}
+void AlturaMedia()
+{
+    int alturaTotal = 0, numMunicipios = 0;
+    municipio *aux = inicio;
+
+    while(aux != final)
+    {
+        alturaTotal += aux -> altitud;
+        numMunicipios++;
+        aux = aux -> siguiente;
+    }
+    alturaTotal += aux -> altitud;
+    numMunicipios++;
+    fout << "Pregunta 2: La altura promedio es " << alturaTotal/numMunicipios << " metros" << endl << endl;
+}
+void Comparaciones()
+{
+    municipio *aux = new municipio;
+    aux = inicio;
+    string masAlto, menosAlto, masDenso, menosDenso, masPoblado, menosPoblado;
+
+    int altMin = inicio -> altitud, altMax = inicio -> altitud;
+    float densidadMax = aux -> poblacion / aux -> superficie;
+    float densidadMin = aux -> poblacion / aux -> superficie;
+    int pobMin = inicio -> poblacion, pobMax = inicio -> poblacion;
+
+    while(aux != final)
+    {
+        if (aux -> altitud <= altMin)
+        {
+            altMin = aux -> altitud;
+            menosAlto = aux -> nombre;
+        }
+        else if (aux -> altitud >= altMax)
+        {
+            altMax = aux -> altitud;
+            masAlto = aux -> nombre;
+        }
+        if (aux -> poblacion / aux -> superficie >= densidadMax)
+        {
+            densidadMax = aux -> poblacion / aux -> superficie;
+            masDenso = aux -> nombre;
+        }
+        else if (aux -> poblacion / aux -> superficie <= densidadMin)
+        {
+            densidadMin = aux -> poblacion / aux -> superficie;
+            menosDenso = aux -> nombre;
+        }
+        if (aux -> poblacion <= pobMin)
+        {
+            menosPoblado = aux -> nombre;
+            pobMin = aux -> poblacion;
+        }
+        else if (aux -> poblacion >= pobMax)
+        {
+            masPoblado = aux -> nombre;
+            pobMax = aux -> poblacion;
+        }
+        aux = aux -> siguiente;
+    }
+    if (aux -> altitud <= altMin)
+    {
+        altMin = aux -> altitud;
+        menosAlto = aux -> nombre;
+    }
+    else if (aux -> altitud >= altMax)
+    {
+        altMax = aux -> altitud;
+        masAlto = aux -> nombre;
+    }
+    if (aux -> poblacion / aux -> superficie >= densidadMax)
+    {
+        densidadMax = aux -> poblacion / aux -> superficie;
+        masDenso = aux -> nombre;
+    }
+    else if (aux -> poblacion / aux -> superficie <= densidadMin)
+    {
+        densidadMin = aux -> poblacion / aux -> superficie;
+        menosDenso = aux -> nombre;
+    }
+    if (aux -> poblacion <= pobMin)
+    {
+        menosPoblado = aux -> nombre;
+        pobMin = aux -> poblacion;
+    }
+    else if (aux -> poblacion >= pobMax)
+    {
+        masPoblado = aux -> nombre;
+        pobMax = aux -> poblacion;
+    }
+
+    fout << "Pregunta 3: El municipio a mayor altitud es " << masAlto << ", a " << altMax << " metros" << endl << endl;
+    fout << "Pregunta 4: El municipio a menor altitud es " << menosAlto <<  ", a " << altMin << " metros" << endl << endl;
+    fout << "Pregunta 5: El municipio mas poblado es " << masPoblado << ", con " << pobMax << " habitantes" << endl << endl;
+    fout << "Pregunta 6: El municipio menos poblado es " << menosPoblado << ", con " << pobMin << " habitantes" << endl << endl;
+    fout << "Pregunta 7: El municipio mas denso es " << masDenso <<  ", con una densidad de " << densidadMax << endl << endl;
+    fout << "Pregunta 8: El municipio menos denso es " << menosDenso <<  ", con una densidad de " << densidadMin << endl << endl;
+}
+void VecesPobMadrid()
+{
+    municipio *aux = inicio;
+    float pobMadrid = 6507184, pobAvila = 0;
+    while(aux != final)
+    {
+        pobAvila += aux -> poblacion;
+        aux = aux -> siguiente;
+    }
+    pobAvila += aux -> poblacion;
+    fout << "Pregunta 9: La poblacion de Madrid es " << pobMadrid / pobAvila  << " veces la de Avila" << endl << endl;
+}
+void CarentaHabs900m()
+{
+    fout << "Pregunta 10: A continuacion una lista con los municipios a mas de 900m con mas de 40 habitantes:" << endl;
+
+    municipio *aux = inicio;
+    int i = 1;
+    while(aux != final && i <= 10)
+    {
+        if(aux -> poblacion > 40)
+        {
+            if (aux -> altitud > 900)
+            {
+                fout << i << ". " << aux -> nombre << " (" << aux -> poblacion << " habitantes, " << aux -> altitud << "m)" << endl;
+                i++;
+            }
+        }
+        aux = aux -> siguiente;
     }
 }
+
 int main()
 {
-    numeroAlumnos = 0;
     LeerArchivo();
-    Mostrar();
-    AlumnoMasViejo();
-    CumplesEnNoviembre();
-    ExpedientesImpar();
-    ExpedientesPar();
-    PorcentajeEntregadosA1();
-    ListaPresencialidad();
+    //Mostrar();
+    AlturaMedia();
+    Comparaciones();
+    VecesPobMadrid();
+    CarentaHabs900m();
 }
+/*1. Cuantos municipios hay
+ * 2. Altura promedio
+ * 3. municipio mas alto
+ * 4. municipio mas bajo
+ * 5. municipio menos poblado
+ * 6. municipio mas poblado
+ * 7. municipio mas denso
+ * 8. municipio menos denso
+ * 9. Cuantas veces supera la Comunidad de Madrid a la Provincia de Avila
+ * 10. imprimir los 10 municipios con mas de 40 habs y a mas de 900m altura*/
